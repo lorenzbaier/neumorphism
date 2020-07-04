305ed3bc2737dda3b9096bc1a5344b3b29bc0a44
lorenzbaier <lorenz.baier10101@gmail.com>
2020-07-05 01:23:19 +0200

    Something is there... :)

---
+++ script.js
@@ -0,0 +1,62 @@
+window.onload = function() {
+
+    /*var slider = document.getElementById("radiusSlider");
+    setBorderRadius("circle", slider.value/10);
+
+    slider.oninput = function() {
+        setBorderRadius("circle", this.value/10);
+    }*/
+
+    document.onmousemove = mouseControl;
+
+}
+
+function mouseControl(event) {
+    var percentage = event.clientX/document.body.clientWidth;
+    setBorderRadius("circle", percentage*50);
+}
+
+function setBorderRadius(className, radius) {
+    radius = radius
+    var toChange = Array.from(document.getElementsByClassName(className));
+
+    toChange.forEach(e => {
+        e.style.borderRadius = radius + "%";
+    });
+
+    setBackground(hslToHex(radius*7.2, 100, 50));
+}
+
+function setBackground(bg) {
+    var root = document.documentElement;
+    root.style.setProperty("--bg-color", bg);
+}
+
+function hslToHex(h, s, l) {
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
